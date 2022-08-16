2a471474b3dc9868d77dbfbe17c8ddce7ce8944c
Bokeron8 <franco123rex777@gmail.com>
2022-08-15 23:03:01 -0300

    Finish exercise

--- javascript/exercises/06_leapYears/leapYears.js
+++ javascript/exercises/06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeap;
+    isLeap = ((year%4 == 0 && year%100 != 0) || year%400 == 0); 
+    return isLeap
 };
 
 // Do not edit below this line
