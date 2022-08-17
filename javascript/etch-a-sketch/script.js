560ce2078d53f834dc499b78cdc7208edf856fb3
Bokeron8 <franco123rex777@gmail.com>
2022-08-17 10:16:49 -0300

    Add painting, change grid size, reset button and styling

--- javascript/etch-a-sketch/script.js
+++ javascript/etch-a-sketch/script.js
@@ -1,11 +1,25 @@
 const gridElement = document.getElementById('grid');
-
-function createGrid(row){
+const resetBtn = document.getElementById('reset-grid-btn');
+let gridSizeElement = document.getElementById('grid-size');
+resetBtn.addEventListener('click', ()=>createGrid());
+createGrid();
+function createGrid(){
+    let row = Math.min(gridSizeElement.value, 100);
     gridElement.style.setProperty('--grid-rows', row);
     gridElement.style.setProperty('--grid-cols', row);
+    gridElement.innerHTML = '';
     for (let i = 0; i<(row*row); i++){
-        let block = document.createElement('div')
-        block.classList.add('block')
-        gridElement.appendChild(block)
+        let block = document.createElement('div');
+        block.classList.add('block');
+        addChangeColor(block);
+        gridElement.appendChild(block);
     }
+}
+function addChangeColor(element){
+    element.addEventListener('mouseenter', (event) => {
+        let block = event.target;
+        opacity = parseFloat(window.getComputedStyle(block,null).getPropertyValue('opacity'));
+        block.style.opacity = 0.1+opacity;
+        block.style.backgroundColor = 'rgb(0, 0, 0)';
+    });
 }
