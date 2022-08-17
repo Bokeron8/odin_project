36f07db9120ba90c68b77d15d16e241f78c1af68
Bokeron8 <franco123rex777@gmail.com>
2022-08-17 13:39:56 -0300

    Finish Exercise

--- javascript/exercises/12_findTheOldest/findTheOldest.js
+++ javascript/exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peoples) {
+    const oldest = peoples.reduce((previusValue, currentValue)=>{
+        const year = new Date().getFullYear();
+        previusAge = 'yearOfDeath' in previusValue? previusValue.yearOfDeath - previusValue.yearOfBirth: year - previusValue.yearOfBirth;
+        currentAge = 'yearOfDeath' in currentValue? currentValue.yearOfDeath - currentValue.yearOfBirth: year - currentValue.yearOfBirth;
+        return previusAge > currentAge? previusValue: currentValue;
+    });
+    return oldest
 };
 
 // Do not edit below this line
