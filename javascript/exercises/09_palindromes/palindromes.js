798cefa147632bdd4aceaa2c978b9b5e96861b1b
Bokeron8 <franco123rex777@gmail.com>
2022-08-17 11:51:19 -0300

    Finish exercise

--- javascript/exercises/09_palindromes/palindromes.js
+++ javascript/exercises/09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    letterStr = string.split('').filter((char)=> char.match(/[a-z]/i));
+    modifiedString = letterStr.join('');
+    reversedString = letterStr.reverse().join('');
+    return reversedString == modifiedString
 };
 
 // Do not edit below this line
